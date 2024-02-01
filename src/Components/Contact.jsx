f5675de92472e5d4176fba64f5c58bf3dcba9712
Vicky Gidvir <vicky.gidvir@pies.io>
2024-02-01 17:44:13 +0530

    ui fixes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,14 +8,14 @@ const Contact = () => {
 				<h2 className='heading'>
 					Get In Touch
 				</h2>
-				<div className='flex lg:flex-row flex-col gap-5'>
+				<div className='flex lg:flex-row  flex-col gap-5 lg:gap-10'>
 
-					<div data-aos="fade-down" data-aos-delay={'400'} className='flex flex-col gap-3 w-[60%] bg-[url("https://jthemes.net/themes/html/bolby/demo/images/map-light.png")] bg-no-repeat'>
+					<div data-aos="fade-down" data-aos-delay={'400'} className='flex flex-col gap-3 lg:w-[60%] h-48 w-full bg-[url("https://jthemes.net/themes/html/bolby/demo/images/map-light.png")] bg-no-repeat'>
 						<h3 className='text-white text-2xl font-bold'>Let's talk about everything!</h3>
 						<p className='font-light text-white'>Don't like forms? Send me an <a href="mailto:vickygidvir@gmail.com" className='text-red-color hover:text-white cursor-pointer transition ease-linear duration-300'>email. 👋</a></p>
 					</div>
 
-					<div data-aos="fade-up" data-aos-delay={'500'} className='flex flex-col gap-10 w-full'>
+					<div data-aos="fade-up" data-aos-delay={'500'} className='flex flex-col gap-5 lg:gap-10 w-full'>
 						<div className='flex gap-5'>
 							<input type="text" placeholder='Your Name' className='input' />
 							<input type="text" placeholder='Email Address' className='input' />
