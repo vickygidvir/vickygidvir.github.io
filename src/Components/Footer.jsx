4d84497de32d762a28b0e5afeb8635804878bb5b
Vicky Gidvir <vicky.gidvir@pies.io>
2024-07-15 22:25:31 +0530

    fixes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import ReactLogo from './ReactLogo';
 
 const Footer = () => {
 	return (
-		<div className='lg:hidden text-grey-color text-sm flex space-x-1 items-center justify-center mb-5'>
+		<div data-aos="fade-up" className='lg:hidden text-grey-color text-sm flex space-x-1 items-center justify-center mb-5'>
 			<span >
 				© 2024 Vix | Crafted with React </span>
 			<ReactLogo />
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -14,22 +14,22 @@ const sidebarItems = [{
 	name: "Home",
 	path: "#home",
 	icon: <IoHomeOutline />
-},{
+}, {
 	id: 2,
 	name: "About",
 	path: "#about",
 	icon: <IoIosPerson />
-},{
+}, {
 	id: 3,
 	name: "Experience",
 	path: "#experience",
 	icon: <BsSuitcaseLgFill />
-},{
+}, {
 	id: 4,
 	name: "Projects",
 	path: "#projects",
 	icon: <IoLayersOutline />
-},{
+}, {
 	id: 5,
 	name: "Contact",
 	path: "#contact",
@@ -39,8 +39,13 @@ const sidebarItems = [{
 const Sidebar = ({ menuOpen, toggleMenu }) => {
 	const [currentMenuItem, setCurrentMenuItem] = useState(0);
 
+	const handleMenuItemClick = (index) => {
+		setCurrentMenuItem(index);
+		toggleMenu();
+	};
+
 	return (
-		<div className={`fixed top-0 left-0 h-screen bg-[#081328] p-8 z-50 transform lg:translate-x-0 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out w-full lg:w-1/5 lg:flex lg:flex-col  lg:justify-between lg:items-center border-r border-[#5B5A6A] flex flex-col justify-between items-center`}>
+		<div className={`fixed top-0 left-0 h-screen bg-[#081328] p-8 z-50 transform lg:translate-x-0 ${menuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out w-full lg:w-1/5 lg:flex lg:flex-col lg:justify-between lg:items-center border-r border-[#5B5A6A] flex flex-col justify-between items-center`}>
 			<IoClose onClick={toggleMenu} className={`text-white absolute top-5 right-6 cursor-pointer lg:hidden ${menuOpen ? '' : 'hidden'}`} size={35} />
 			<div className='flex flex-col items-center justify-center gap-3'>
 				<img className='h-40' src={photo} />
@@ -55,15 +60,13 @@ const Sidebar = ({ menuOpen, toggleMenu }) => {
 			</div>
 			<ul className='flex flex-col gap-5'>
 				{
-					sidebarItems.map((item, index) => {
-						return (
-							<li key={item.id} onClick={() => setCurrentMenuItem(index)}>
-								<a href={item.path} className={`text-[17px] font-bold ${currentMenuItem === index ? 'text-yellow-color' : 'text-white'} flex items-center gap-4 hover:text-yellow-color hover:cursor-pointer`}>
-									<span className='text-yellow-color text-[1.2rem]'>{item.icon}</span>{item.name}
-								</a>
-							</li>
-						);
-					})
+					sidebarItems.map((item, index) => (
+						<li key={item.id} onClick={() => handleMenuItemClick(index)}>
+							<a href={item.path} className={`text-[17px] font-bold ${currentMenuItem === index ? 'text-yellow-color' : 'text-white'} flex items-center gap-4 hover:text-yellow-color hover:cursor-pointer`}>
+								<span className='text-yellow-color text-[1.2rem]'>{item.icon}</span>{item.name}
+							</a>
+						</li>
+					))
 				}
 			</ul>
 			<div className='text-grey-color text-sm flex space-x-1 items-center justify-center'>
